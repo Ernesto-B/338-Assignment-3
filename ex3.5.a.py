49b3c17360fd289d602be3799df2fc8f41571ca0
JReeseSanchez <113329309+JReeseSanchez@users.noreply.github.com>
2023-03-12 00:14:43 -0700

    Printing graphs

--- ex3.5.a.py
+++ ex3.5.a.py
@@ -1,5 +1,6 @@
 import random
 import time
+import matplotlib.pyplot as plt
 
 searchTimes = []
 
@@ -14,20 +15,35 @@ def testArray():
     arr = sorted(random.sample(range(n*10), n))
     return arr
 
-
-
+def totalTime (inputTime):
+    # combinding the times
+    totalTime = []
+    index = 0
+    for i in inputTime:
+        timeStamp = 0
+        for x in range(index):
+            timeStamp += inputTime[x]
+        totalTime.append(timeStamp)
+        index += 1
+    
+    return totalTime
 
 startTest = time.perf_counter()
 
 for i in range(1000):
-    test = testArray
+    test = testArray()
     startSearch = time.perf_counter()
-    linear_search(test, random(0, 10000))
+    linear_search(test, random.randint(0, 10000))
     endSearch = time.perf_counter()
 
     searchTimes.append(endSearch-startSearch)
 
 endTest = time.perf_counter()
 
+timeStamps = totalTime(searchTimes)
 
-print(str(testArray()))
+plt.scatter(timeStamps, searchTimes)
+plt.xlabel('Time Elapsed')
+plt.ylabel('Search Time')
+plt.title('Time it took to search for input')
+plt.show()
--- ex3.5.b.py
+++ ex3.5.b.py
@@ -1,5 +1,8 @@
 import random
 import time
+import matplotlib.pyplot as plt
+
+searchTimes = []
 
 def binary_search(arr, target):
     left = 0
@@ -14,10 +17,36 @@ def binary_search(arr, target):
             right = mid - 1
     return -1
 
-def experiment():
+def testArray():
     n = 1000
     arr = sorted(random.sample(range(n*10), n))
-    target = random.randint(0, n*10-1)
-    linear_times = []
-    binary_times = []
-    num_trials = 100
+    return arr
+
+def totalTime (inputTime):
+    # combinding the times
+    totalTime = []
+    index = 0
+    for i in searchTimes:
+        timeStamp = 0
+        for x in range(index):
+            timeStamp += inputTime[x]
+        totalTime.append(timeStamp)
+        index += 1
+    
+    return totalTime
+
+for i in range(1000):
+    test = testArray()
+    startSearch = time.perf_counter()
+    binary_search(test, random.randint(0, 10000))
+    endSearch = time.perf_counter()
+
+    searchTimes.append(endSearch-startSearch)
+
+timeStamps = totalTime(searchTimes)
+
+plt.scatter(timeStamps, searchTimes)
+plt.xlabel('Time Elapsed')
+plt.ylabel('Search Time')
+plt.title('Time it took to search for input')
+plt.show()
