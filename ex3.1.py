213ec4cb5a043fdc99c94934185f0f20c434f459
JReeseSanchez <113329309+JReeseSanchez@users.noreply.github.com>
2023-03-12 00:51:30 -0700

    Thats all I can do :|

--- ex3.1.py
+++ ex3.1.py
@@ -11,7 +11,6 @@ def isEmpty(stack):
 
 def push(stack, item):
     stack.append(item)
-    print(item + " pushed to stack ")
 
 def pop(stack):
     if (isEmpty(stack)):
@@ -53,13 +52,10 @@ def compute(op, inNum1, inNum2):
 
 def evaluate(stack):
     index = len(stack)
-    hold = ""
+    hold = str()
     for i in reversed(stack):
 
-        print("We Go Agian with: " +str(stack) + "  and were at: " + str(i))
         if checkBracket(i, ')'):
-
-            print("*find index: " + str(len(stack)) + " - (" + str(index) + "+ 3 = " + str(len(stack) - (index + 3)))
 
             if checkBracket(stack[len(stack) - (index + 3)], '('):
                 
@@ -67,23 +63,17 @@ def evaluate(stack):
                 num1 = pop(stack)
                 op = pop(stack)
 
-                print("calculate: " + num1 + op + num2)
-
                 result = compute(op, num1, num2)
                 
-                push(stack, str(result + ")"))
-                print(str(stack))
+                push(stack, str(result))
 
                 if bool(hold) != "":
-                    print("This is in hold: " + str(hold))
                     push(stack, hold)
                     hold = str()
 
             else:
                 hold = pop(stack)
-                print("**putting this on hold: " + str(hold))
 
-        print("stack length: " +str(len(stack)))
         if len(stack) == 1:
             return str(stack[0])
         
@@ -95,7 +85,7 @@ def evaluate(stack):
 if __name__ == '__main__':
     conInput = sys.argv[1]
     stack = createStack(conInput)
-    print(stack)
     result = evaluate(stack)
-    print("input: " + conInput + " = " + result)
+    result = result.replace(")","")
+    print(result)
     
